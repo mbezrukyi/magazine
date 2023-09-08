12d9307781988ecfdb43d3b6008f5618aa3481fc
Mykola Bezrukyi <mykola_bezrukyi@epam.com>
2023-09-08 18:53:07 +0300

    init project extensions

--- magazine/__init__.py
+++ magazine/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
 
+from .extensions import db, login_manager
+
 
 def create_app(config_file='settings.py') -> Flask:
     app = Flask(__name__)
 
     app.config.from_pyfile(config_file)
+
+    db.init_app(app)
+
+    login_manager.init_app(app)
 
     return app
